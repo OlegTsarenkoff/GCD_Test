7f229d4d56ec57f45d7a849bcc7b77fe5e7cfa51
Oleg Tsarenkoff <tsarenkow@icloud.com>
2021-10-12 17:37:08 +0300

    delay!!! :)

--- GCD_Test/SecondViewController.swift
+++ GCD_Test/SecondViewController.swift
@@ -30,15 +30,48 @@ class SecondViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         fetchImage()
+        delay(3) {
+            self.loginAlert()
+        }
+    }
+    
+    fileprivate func delay(_ delay: Int, closure: @escaping () -> ()) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(delay)) {
+            closure()
+        }
+    }
+    
+    fileprivate func loginAlert() {
+        let ac = UIAlertController(title: "Зарегистрирован?", message: "Введите ваше логин и пароль", preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        let cancelAction = UIAlertAction(title: "Отмена", style: .cancel, handler: nil)
+        
+        ac.addAction(okAction)
+        ac.addAction(cancelAction)
+        
+        ac.addTextField { (userNameTF) in
+            userNameTF.placeholder = "Введите логин"
+        }
+        ac.addTextField { (userPasswordF) in
+            userPasswordF.placeholder = "Введите пароль"
+            userPasswordF.isSecureTextEntry = true
+        }
+        
+        self.present(ac, animated: true, completion: nil)
     }
     
     fileprivate func fetchImage() {
-        imageURL = URL(string: "https://klike.net/uploads/posts/2020-01/1578212659_1.jpeg")
-        
         activityIndicator.startAnimating()
         activityIndicator.isHidden = false
         
-        guard let url = imageURL, let imageData = try? Data(contentsOf: url) else { return }
-        self.image = UIImage(data: imageData)
+        imageURL = URL(string: "https://klike.net/uploads/posts/2020-01/1578212659_1.jpeg")
+        let queue = DispatchQueue.global(qos: .utility)
+        queue.async {
+            guard let url = self.imageURL, let imageData = try? Data(contentsOf: url) else { return }
+            DispatchQueue.main.async {
+                self.image = UIImage(data: imageData)
+            }
+        }
     }
 }
